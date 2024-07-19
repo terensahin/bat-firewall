ac2b35ccc29d68b51c3198d25f760c53d958f281
baranbologur <baranbologur@hotmail.com>
2024-07-19 10:38:45 +0300

    log levels are added

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 cc = gcc
  
-all: daemon.c c_vector.o common.o cli.c
-	$(cc) -o airdaemon daemon.c c_vector.o common.o
+all: daemon.c cli.c c_vector.o common.o log.o
+	$(cc) -o airdaemon daemon.c c_vector.o common.o log.o
 	$(cc) -o cli cli.c common.o
  
 c_vector.o: c_vector.c c_vector.h
@@ -9,6 +9,9 @@ c_vector.o: c_vector.c c_vector.h
 
 common.o: common.c common.h
 	$(cc) -c common.c common.h
+
+log.o: log.c log.h
+	$(cc) -c log.c log.h
     
 clean:
-	rm -rf airdaemon daemon.o c_vector.o c_vector.h.gch daemon.log error.log backup.log cli common.o common.h.gch
+	rm -rf airdaemon daemon.o c_vector.o log.o c_vector.h.gch daemon.log error.log backup.log cli common.o common.h.gch log.h.gch
--- daemon.c
+++ daemon.c
@@ -14,10 +14,10 @@
 #include <sys/un.h>
 #include "c_vector.h"
 #include "common.h"
+#include "log.h"
 
-static const char *LOG_FILE = "/home/ahmet/work/airties/daemon/daemon.log";
-static const char *ERR_FILE = "/home/ahmet/work/airties/daemon/error.log";
-static const char *BACKUP_FILE = "/home/ahmet/work/airties/daemon/backup.log";
+static const char *LOG_FILE = "/home/baranbolo/Desktop/platform_i/daemon.log";
+static const char *BACKUP_FILE = "/home/baranbolo/Desktop/platform_i/backup.log";
 
 #define SV_SOCK_PATH "/tmp/ud_ucase"
 #define BUF_SIZE 1024
@@ -25,93 +25,82 @@ static const char *BACKUP_FILE = "/home/ahmet/work/airties/daemon/backup.log";
 #define MAXIMUM_MESSAGE_COUNT 10
 
 static FILE *logfp;
-
+// static FILE *logfp2;
 int *vector;
 
-ssize_t log_message(const char *format)
-{
-    return fprintf(logfp, "%s", format); /* Writes to opened log file */
-}
+// char *get_time()
+// {
+//     time_t t;
+//     char *str_t;
+//     time(&t);
+//     str_t = ctime(&t);
+//     str_t[strlen(str_t) - 1] = '\0';
+//     return str_t;
+// }
 
-void log_open(const char *logFilename, char* open_mode)
-{
-    mode_t m;
-
-    m = umask(077);                  /* To recover old mask */
-    logfp = fopen(logFilename, open_mode); /* File is opened with permissions 700 */
-    umask(m);
-
-    if (logfp == NULL) /* If opening the log fails */
-        exit_with_log("Error opening log");
-
-    setbuf(logfp, NULL); /* Disable stdio buffering */
-}
-
-void log_close(void)
-{
-    fclose(logfp);
-}
-
-
-
+// ssize_t log_message(const char *format)
+// {
+//     return fprintf(logfp2, "[%-20s] : %s", format, get_time()); /* Writes to opened log file */
+// }
+ 
+// void log_open(const char *logFilename, char* open_mode)
+// {
+//     mode_t m;
+ 
+//     m = umask(077);                  /* To recover old mask */
+//     logfp2 = fopen(logFilename, open_mode); /* File is opened with permissions 700 */
+//     umask(m);
+ 
+//     if (logfp2 == NULL) /* If opening the log fails */
+//         exit(EXIT_FAILURE);
+ 
+//     setbuf(logfp2, NULL); /* Disable stdio buffering */
+// }
+ 
+// void log_close(void)
+// {
+//     fclose(logfp2);
+// }
 
 void backup_shutdown(){
-    log_open(BACKUP_FILE, "w");
-    char tmpbuf[128];
-    snprintf(tmpbuf, 16, "%d\n", vector_get_size(vector));
-    log_message(tmpbuf);
+    FILE *file = fopen(BACKUP_FILE, "w");
+
+    fprintf(file, "%d\n", vector_get_size(vector));
     for(int i = 0; i < vector_get_size(vector); i++){
         student *tmpstd = vector_at(vector, i);
-        snprintf(tmpbuf, 128, "%s,%d,%f\n", tmpstd->name, tmpstd->id, tmpstd->grade);
-        log_message(tmpbuf);
+        fprintf(file, "%s,%d,%f\n", tmpstd->name, tmpstd->id, tmpstd->grade);
     }
-    log_close();
     vector_free(vector);
+    fclose(file);
+    fclose(logfp);
 }
 
 /*  
     Read the backup file and fill the vector
 */
 void backup_start(){
-    log_open(LOG_FILE, "a");
     FILE *file = fopen(BACKUP_FILE, "a+");
     if (file == NULL) {
         return;
     }
 
-    log_message("Backup file opened\n");
-
     int vector_size;
     if (fscanf(file, "%d\n", &vector_size) != 1) {
         fclose(file);
-        log_close();
         return;
     }
     int readed_number;
     student tmpstd;
     for(int i = 0; i < vector_size; i++){
-        log_message("Reading backup file\n");
         readed_number = fscanf(file, "%[^,],%d,%f\n", tmpstd.name, &tmpstd.id, &tmpstd.grade);
         if(readed_number != 3){
             break;
         }
-        log_message("Readed student\n");
         vector = vector_push_back(vector, &tmpstd);
     }
 
-    log_message("Backup file readed successfully\n");
     fclose(file);
-    log_close(); 
     return;
-}
-
-
-void exit_with_log(char *err){
-    log_open(ERR_FILE, "a");
-    log_message(err);
-    log_close();
-    backup_shutdown();
-    exit(EXIT_FAILURE);
 }
 
 static void skeleton_daemon()
@@ -121,39 +110,42 @@ static void skeleton_daemon()
     pid = fork(); /* This fork is to leave from process group */
 
     if (pid < 0) /* fork error */
-        exit_with_log("Error daemon, Fork");
+        exit(EXIT_FAILURE);
 
     if (pid > 0)
     { /* Parent exits */
-    	exit(EXIT_SUCCESS);
+        exit(EXIT_SUCCESS);
     }
 
     /*child continues*/
     if (setsid() < 0) /* Child process becomes leader of his own session */
-        exit_with_log("Error setting session ID");
+        exit(EXIT_FAILURE);
 
     struct sigaction sa; /* Ignore SIGCHLD and SIGHUP signals */
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = SA_RESTART;
     sa.sa_handler = SIG_IGN;
     if (sigaction(SIGHUP, &sa, NULL) == -1) {
-        exit_with_log("Sigaction");
+        perror("sigaction");
+        exit(EXIT_FAILURE);
     }
     if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-        exit_with_log("Sigaction");
+        perror("sigaction");
+        exit(EXIT_FAILURE);
     }
 
     sigemptyset(&sa.sa_mask); /* Set handler for SIGTERM signal (backup on shutdown) */
     sa.sa_flags = SA_RESTART;
     sa.sa_handler = backup_shutdown;
     if (sigaction(SIGTERM, &sa, NULL) == -1) {
-        exit_with_log("Sigaction");
+        perror("sigaction");
+        exit(EXIT_FAILURE);
     }
 
     pid = fork(); /* This fork is to prevent access to the terminal */
 
     if (pid < 0) /* fork error*/
-        exit_with_log("Error daemon, Fork");
+        exit(EXIT_FAILURE);
 
     if (pid > 0) /* Parent exits */
         exit(EXIT_SUCCESS);
@@ -176,11 +168,11 @@ static void skeleton_daemon()
 
     int fd = open("/dev/null", O_RDWR); /* 0,1,2 file desciptors are pointed to null, so functions using stdin or stdout does not generate error */
     if (fd != STDIN_FILENO)             /* 'fd' should be 0 */
-        exit_with_log("Error File descriptor");
+        exit(EXIT_FAILURE);
     if (dup2(STDIN_FILENO, STDOUT_FILENO) != STDOUT_FILENO)
-        exit_with_log("Error File descriptor");
+        exit(EXIT_FAILURE);
     if (dup2(STDIN_FILENO, STDERR_FILENO) != STDERR_FILENO)
-        exit_with_log("Error File descriptor");
+        exit(EXIT_FAILURE);
 }
 
 int create_socket()
@@ -191,14 +183,15 @@ int create_socket()
     socket_fd = socket(AF_UNIX, SOCK_DGRAM, 0);
     if (socket_fd == -1)
     {
-	exit_with_log("Error creating socket!");
+        perror("Error creating socket!");
+        exit(EXIT_FAILURE);
     }
 
     if (strlen(SV_SOCK_PATH) > sizeof(socket_address.sun_path) - 1)
-        exit_with_log("Error Socket");
+        exit(EXIT_FAILURE);
 
     if (remove(SV_SOCK_PATH) == -1 && errno != ENOENT)
-        exit_with_log("Error Socket");
+        exit(EXIT_FAILURE);
 
     memset(&socket_address, 0, sizeof(struct sockaddr_un)); /* Cleared the socket_address */
     socket_address.sun_family = AF_UNIX;                    /* UNIX domain address */
@@ -208,9 +201,12 @@ int create_socket()
     {
         if (errno == EADDRINUSE)
         {
-            exit_with_log("Error Socket in use");
+            perror("socket in use");
+            exit(EXIT_FAILURE);
         }
-        exit_with_log("Error Binding");
+        perror("binding error ");
+
+        exit(EXIT_FAILURE);
     }
 
     // change
@@ -224,14 +220,12 @@ void execute_command(daemon_command command, char* response, ssize_t *command_le
     switch (command.command_type)
     {
     case add:
-	    ;
         student tmpstd = command.student_info;
         vector = vector_push_back(vector, &tmpstd);
         snprintf(response, BUF_SIZE, "Successfully added student: Name: %s, ID: %d, Grade: %f\n" ,\
             command.student_info.name, command.student_info.id, command.student_info.grade);
         break;
     case del:
-	;
         int delete_id = command.student_info.id;
         for(int i = vector_get_size(vector) - 1; i >= 0; i--){
             student *tmpstd = vector_at(vector, i);
@@ -245,8 +239,8 @@ void execute_command(daemon_command command, char* response, ssize_t *command_le
         snprintf(response, BUF_SIZE, "All students:\n" );
         for(int i = 0; i < vector_get_size(vector); i++){
             student *tmpstd = vector_at(vector, i);
-            char tmpbuf[128];
-            snprintf(tmpbuf, 128, "Name: %s, ID: %d, Grade: %f\n", tmpstd->name, tmpstd->id, tmpstd->grade);
+            char tmpbuf[BUF_SIZE];
+            snprintf(tmpbuf, BUF_SIZE, "Name: %s, ID: %d, Grade: %f\n", tmpstd->name, tmpstd->id, tmpstd->grade);
             strcat(response, tmpbuf);
         }
         break;
@@ -262,14 +256,40 @@ void execute_command(daemon_command command, char* response, ssize_t *command_le
     return;
 }
 
+FILE *set_log_levels(){
+    log_set_level(0);
+    log_set_quiet(1);
+    FILE *fp;
+    fp = fopen(LOG_FILE, "ab");
+    if(fp == NULL){
+        return NULL;
+    }
+    log_add_fp(fp, 0);
+
+    return fp;
+}
+
 int main(int argc, char *argv[])
 {
     skeleton_daemon();
+
+    // log_open(LOG_FILE, "a");
+
+    // log_message("hello\n");
+    FILE *logfp = set_log_levels();
+    if (logfp == NULL){
+        exit(EXIT_FAILURE);
+    }
+    // log_message("hello2\n");
 
     vector = vector_initialize(vector, sizeof(student), NULL);
     backup_start();
 
     int socket_fd = create_socket();
+
+    log_trace("HELLO");
+    log_info("HELLO");
+    log_error("HELLO");
 
     struct sockaddr_un claddr;
     socklen_t len = sizeof(struct sockaddr_un);
@@ -278,17 +298,18 @@ int main(int argc, char *argv[])
     char response[BUF_SIZE];
     while (1) {
         if (recvfrom(socket_fd, &command, sizeof(daemon_command), 0, (struct sockaddr *) &claddr, &len) == -1)
-            exit_with_log("Error recieveing info");
+            exit(EXIT_FAILURE);
 
         execute_command(command, response, &command_bytes);
 
         if (sendto(socket_fd, response, command_bytes, 0, (struct sockaddr *) &claddr, len) != command_bytes){
-            exit_with_log("Error sending info");
+            exit(EXIT_FAILURE);
         }
 
         if(command.command_type == terminate){
             return EXIT_SUCCESS;
         }
     }
+    fclose(logfp);
     return EXIT_SUCCESS;
 }
